5173374c0c392ad8f1cf98624ee86a9ce2b6dca3
cecilia-mt <speedofsound_14@hotmail.com>
2017-09-28 14:29:45 -0500

    Refactoring
    I could not execute it, but I think it is one of the ways in which it
    could be better

--- Step2/chat.js
+++ Step2/chat.js
@@ -1,72 +1,57 @@
-<script type="text/javascript">
-  var url = window.location.href;
+var url = window.location.href;
   var locale = url.split("/")[3];
-  var onlineGreeting, offlineGreeting;
+  var onlineGreeting, offlineGreeting, department;
 
-  if(locale === "es") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+   window.$zopim||(function(d,s){var z=$zopim=function(c){
+   z._.push(c)},$=z.s=
+   d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+   _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+   $.src='//v2.zopim.com/?';z.t=+new Date;$.
+   type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
 
-    onlineGreeting = "Habla con nosotras";
-    offlineGreeting = 'Deja un mensaje';
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
+   switch(locale){
 
-  } 
-  else if(locale === "pt-pt") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Fale com a gente";
-    offlineGreeting = 'Deixe uma mensagem';
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
-  } 
-  else if(locale === "fr") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Parlez-nous";
-    offlineGreeting = 'Laissez un message';
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
-  } 
-  else {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Chat with us";
-    offlineGreeting = "Leave us a message";
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
-  }
-</script>
+    case "es":
+      onlineGreeting = 'Habla con nosotras';
+      offlineGreeting = 'Nuestros consejeros no están disponibles en este momento, por favor deje un mensaje y nos pondremos en contacto con usted pronto';
+      department = "spanish";
+      break;
+
+    case "pt-pt":
+      onlineGreeting = 'Fale com a gente';
+      offlineGreeting = 'Nossos conselheiros não estão disponíveis no momento, por favor deixe uma mensagem e entraremos em contato com você em breve';
+      department = "portuguese";
+      break;
+
+    case "fr":
+      onlineGreeting = 'Parlez-nous';
+      offlineGreeting = 'Nos conseillers ne sont pas disponibles pour le moment, veuillez laisser un message et nous vous contacterons bientôt';
+      department = "french";
+      break;
+
+    case "ch":
+      onlineGreeting = '跟我們說話';
+      offlineGreeting = '我們的顧問目前不可用，請留言，我們將盡快回复您';
+      department = "chinese"
+      break;
+
+    case "rs":
+      onlineGreeting = 'Поговорите с нами';
+      offlineGreeting = 'Наши консультанты в настоящее время недоступны, пожалуйста, оставьте сообщение, и мы скоро свяжемся с вами';
+      break;
+
+    default 
+      onlineGreeting = 'Chat with us';
+      offlineGreeting = "Our counselors are not available at the moment, please leave a message and we will contact you soon";
+      department = "english";  
+      break;
+
+   }
+
+ $zopim(function() {
+   $zopim.livechat.setGreetings({
+   'online': onlineGreeting,
+   'offline': offlineGreeting
+   });
+   $zopim.livechat.departments.filter(department);
+ });
